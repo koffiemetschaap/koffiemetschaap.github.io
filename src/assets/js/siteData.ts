8e0da08bbb0f3659b8eeecf513709d1716786be7
Wouter Schaap <hallo@koffiemetschaap.nl>
2025-06-19 14:20:36 +0200

    Update availability

--- src/assets/js/siteData.ts
+++ src/assets/js/siteData.ts
@@ -7,5 +7,5 @@ export const site = {
         "Hey hallo, ik ben Wouter Schaap. Vanuit Koffie met Schaap help ik bedrijven bij het vertalen van hun ideeën of designs naar robuuste Front-end code met een flexibele CSS architectuur die toekomstbestendig en toegankelijk is voor iedereen.",
     baseurl: "",
     url: "",
-    available: "Januari 2025",
+    available: "Q3 2025",
 };
